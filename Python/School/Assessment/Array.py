e1ed38271f5ec518d72af509c9cd97d166eafe8a
Thierry <Wolken049@gmail.com>
2026-02-01 19:22:55 +0200

    Update gitignore

---
+++ Python/School/Assessment/Array.py
@@ -0,0 +1,9 @@
+import random
+from PrintArray import PrintArray
+
+Numb = []
+
+for i in range(20):
+    Numb.append(random.randint(1, 100))
+    
+print(Numb)
---
+++ Python/School/Assessment/PrintArray.py
@@ -0,0 +1,10 @@
+def PrintArray(Numb):
+    space = " "
+    Numbstr = []
+    
+    for i in Numb - 1:
+        if Numbstr == None:
+            Numbstr.append(str(Numb[i]))
+        else:
+            Numbstr.append(Numbstr[i-1] + Numbstr[i])
+
