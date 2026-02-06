15f1a9922800f1f1c95fdda30c63b7c340ea2b79
Thierry <Wolken049@gmail.com>
2026-02-06 17:51:30 +0200

    Implementing Java

---
+++ Welcome to Java/src/Arithmatic.java
@@ -0,0 +1,9 @@
+public class Arithmatic{
+    
+    public static void main(String[] args) {
+        System.out.println(3 + 3);
+        System.out.println(3 - 3);
+        System.out.println(3 * 3);
+        System.out.println(3 / 3);
+    }
+}
