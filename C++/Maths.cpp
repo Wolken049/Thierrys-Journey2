aa591ae48c73cbffb28a78ea02da7956967ee890
Thierry <Wolken049@gmail.com>
2026-02-20 09:35:39 +0200

    Fixing math

--- C++/Maths.cpp
+++ C++/Maths.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 int main() {
-    cout << sqrt(4);
-    cout << round(2.6)
-    cout << log(2)
+    cout << sqrt(4) << " ";
+    cout << round(2.6) << " ";
+    cout << log(2) << " ";
+    return 0;
 }
